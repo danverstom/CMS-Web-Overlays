5bb00d97e943928a2f2e9666e04def5e4d3f0a61
TomD53 <tomtomgaming101@gmail.com>
2021-07-20 21:23:08 +0100

    change overlay to say DTT

--- static/js/score_bar.js
+++ static/js/score_bar.js
@@ -11,7 +11,7 @@ const ScoreBar = {
     data() {
         return {
             current_text: "CTF Match Spotlight",
-            text_items: ["CTF Match Spotlight", "Season Two"],
+            text_items: ["CTF Match Spotlight", "Draft Team Tournament"],
             red_name: "Red Team",
             red_score: 0,
             red_logo: "",
